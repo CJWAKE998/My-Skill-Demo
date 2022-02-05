96ca04886e3a7fec3f2345bf4106d8f9add78e01
CJWAKE998 <97641362+CJWAKE998@users.noreply.github.com>
2022-02-04 16:40:47 -0800

    lib + my skill demo

--- MySkillDemo.java
+++ MySkillDemo.java
@@ -1,1 +1,9 @@
 //Not going to edit in github
+public class MySkillDemo{
+
+    public static String str(String strTest){
+        return strTest;
+    }
+
+
+}
---
+++ MySkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MySkillDemoTester {
+    @Test
+    public void strCheck(){
+        assertEquals("Hi", MySkillDemo.str("Hello"));
+    }
+}
